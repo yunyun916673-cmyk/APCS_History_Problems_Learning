db770c2b3984581ec8b1f1db9f016924af79d6ab
yunyun916673-cmyk <yunyun916673@gmail.com>
2026-07-09 21:46:29 +0800

    Create stunt_performance.cpp

---
+++ Easy/stunt_performance/stunt_performance.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using ll=long long;
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n; 
+    cin>>n;
+    ll buil[n];
+    for (ll i=0;i<n;i++) 
+        cin>>buil[i];
+    ll cnt=1;
+    ll ans=1;
+    for (ll i=1;i<n;i++)
+    {
+        if (buil[i]<buil[i-1]) cnt++;
+        else cnt=1;
+        ans=max(ans,cnt);
+    }
+    cout<<ans;
+    return 0;
+}
