bce5efa4941e54126a015ede9c1c1ba91752ad80
yunyun916673-cmyk <yunyun916673@gmail.com>
2026-07-20 16:27:49 +0800

    Update Logical_operators.cpp

--- Easy/Logical_operators/Logical_operators.cpp
+++ Easy/Logical_operators/Logical_operators.cpp
@@ -6,6 +6,8 @@ int main()
     cin.tie(nullptr);
     int a,b,t; cin>>a>>b>>t;
     bool pass=false;
+    a=(a>1?1:a);
+    b=(b>1?1:b);
     if ((a&b)==t) {cout<<"AND"<<"\n";pass=true;}
     if ((a|b)==t) {cout<<"OR"<<"\n";pass=true;}
     if ((a^b)==t) {cout<<"XOR"<<"\n";pass=true;}
