4a7e0a70f33276a0a4bd990bdb4192184d098258
yunyun916673-cmyk <yunyun916673@gmail.com>
2026-07-23 16:40:35 +0800

    Update rat_machine.py

--- Easy/rat_machine/rat_machine.py
+++ Easy/rat_machine/rat_machine.py
@@ -2,8 +2,7 @@ import sys
 import bisect
 input=sys.stdin.readline
 x,n=map(int,input().split())
-foods=list(map(int,input().split()))
-foods.sort()
+foods=sorted((map(int,input().split())))
 pos=bisect.bisect_left(foods,x)
 if pos<n-pos:
 	print(f"{n-pos} {foods[n-1]}")
