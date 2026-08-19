8e2db6950b4d810e7a1e90487f78c94bd6b5ec30
yunyun916673-cmyk <yunyun916673@gmail.com>
2026-08-20 07:35:38 +0800

    Create missing_words_2.cpp

---
+++ medium _high/missing_words/missing_words_2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <string>
+using ll=long long;
+using namespace std;
+ll L; string S,K;
+unordered_set<string> lines;
+void get_lines()
+{
+    ll sz=(ll)S.size();
+    for (ll i=0LL;i+L<=sz;i++)
+    {
+        string s=S.substr(i,L);
+        lines.insert(s);
+    }
+    return;
+}
+void dfs(string s)
+{
+    if ((ll)s.size()==L&&!lines.count(s))
+    {cout<<s;exit(0);}
+    for (char ch:K) dfs(s+ch);
+    return;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin>>K>>L>>S;
+    get_lines();
+    dfs("");
+    return 0;
+}
